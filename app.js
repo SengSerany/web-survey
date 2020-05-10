2b425a81086a97c1236aefc6e92122e2f20990b7
SengSerany <cranyseng@gmail.com>
2020-05-10 15:11:49 +0200

    set initial layout

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
     res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
 let port = 4000;
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('home', {title: 'Survey Homepage'});
+    res.render('home/home', {title: 'Survey Homepage'});
 });
 
 module.exports = router;
