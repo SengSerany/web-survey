a186d58debffff4280a7988763c40e5f7920b26f
SengSerany <cranyseng@gmail.com>
2020-06-10 12:21:54 +0200

    correct bug off ensureAuthenticated function

--- config/auth.js
+++ config/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
     ensureAuthenticated: (req, res, next) => {
         if(req.isAuthenticated()) {
-            return next;
+            return next();
         }
         req.flash('error_msg', 'Veillez-vous connecter pour poursuivre :)');
         res.redirect('/authentification/logIn');
